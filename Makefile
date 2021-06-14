4d6db55231d035efbd0aaa571a63844213772660
Anton-XIX <anton_kiryakov2@mail.ru>
2021-06-14 12:50:26 +0300

    Changed python and postgres images,added 'CheckConstraint' to auction model.

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
 build-dev:
 	make stop-dev
 	docker-compose -f build/dev/docker-compose.yml up -d --build
+
 run-dev:
-	docker-compose  -f build/dev/docker-compose.yml up -d --build
+	docker-compose -f build/dev/docker-compose.yml up -d --build web
+	docker exec -ti dev_web_1 python manage.py makemigrations
+	docker exec -ti dev_web_1 python manage.py migrate
+
 stop-dev:
 	docker-compose -f build/dev/docker-compose.yml down -v
+
+stop-web:
+	docker-compose -f build/dev/docker-compose.yml stop web
+
 logs-dev:
 	docker-compose -f build/dev/docker-compose.yml logs -f
+
 create-admin-dev:
 	docker exec -ti dev_web_1 python manage.py createsuperuser
+
--- backend/auction/models.py
+++ backend/auction/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 
 class Auction(models.Model):
@@ -12,13 +13,22 @@ class Auction(models.Model):
     type = models.CharField(max_length=20, choices=AUCTION_TYPE)
     start_price = models.DecimalField(max_digits=8, decimal_places=2)
     reserve_price = models.DecimalField(max_digits=8, decimal_places=2)
-    update_frequency = models.DurationField()
     duration = models.DurationField()
-    current_price = models.DecimalField(max_digits=8, decimal_places=2)
+    update_frequency = models.DurationField(blank=True, null=True)
+    current_price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return f'{self.type} - {self.pk}'
-    # class Meta:
-    #     constraints = models.UniqueConstraint(fields=['user'], condition=Q(status='DRAFT'), name='unique_draft_user')
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(type='En') & Q(current_price__isnull=False), name='en_auction'
+            ),
+            models.CheckConstraint(
+                check=Q(type='Nl') & Q(update_frequency__isnull=False), name='nl_auction'
+            ),
+        ]
+
 #
 #
