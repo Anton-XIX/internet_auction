8b150017217b3e7e8a6850b8cb486f23e9535f6f
Anton Kiryakov <anton_kiryakov2@mail.ru>
2021-06-30 22:26:46 +0300

    Added installing netcat in docker, refactored lot nested serializer because of frontend troubles with post request.

--- backend/lot/serializers.py
+++ backend/lot/serializers.py
@@ -14,12 +14,26 @@ class OfferSerializer(serializers.ModelSerializer):
 
 
 class LotNestedSerializer(serializers.ModelSerializer):
-    item = ItemSerializer(many=False)
-    auction = AuctionSerializer(many=False)
+    # item = ItemSerializer(read_only=True)
+    # auction = AuctionSerializer(read_only=True)
+    # auction = serializers.JSONField(write_only=True)
+    # item = serializers.JSONField(write_only=True)
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(source='item.title')
+    description = serializers.CharField(source='item.description')
+    photo = serializers.FileField(source='item.photo')
+    type = serializers.CharField(source='auction.type')
+    start_price = serializers.CharField(source='auction.start_price')
+    reserve_price = serializers.CharField(source='auction.reserve_price')
+    duration = serializers.CharField(source='auction.duration')
+    current_price = serializers.CharField(source='auction.current_price')
+    update_frequency = serializers.CharField(source='auction.update_frequency', required=False)
+    buy_now_price = serializers.CharField(source='auction.buy_now_price')
 
     class Meta:
         model = Lot
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'photo', 'type', 'start_price', 'reserve_price',
+                  'duration', 'current_price', 'update_frequency', 'buy_now_price', 'is_active']
 
     @transaction.atomic
     def create(self, validated_data):
@@ -27,7 +41,7 @@ class LotNestedSerializer(serializers.ModelSerializer):
         auction_data = validated_data.pop('auction')
         item = Item.objects.create(**item_data)
         auction = Auction.objects.create(**auction_data)
-        lot = Lot.objects.create(item=item, auction=auction, **validated_data)
+        lot = Lot.objects.create(item=item, auction=auction, is_active=True)
         return lot
 
 
--- backend/lot/views.py
+++ backend/lot/views.py
@@ -6,14 +6,13 @@ from rest_framework.filters import OrderingFilter
 
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin
-from rest_framework.parsers import MultiPartParser, FileUploadParser
-
+from rest_framework.parsers import MultiPartParser, FormParser
 '''Search field and type(=, ^ etc.) ?'''
 
 
 class LotListCreateRetrieveViewSet(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin):
-    # parser_classes = [MultiPartParser]
-    queryset = Lot.objects.select_related('auction', 'item')
+    # parser_classes = [MultiPartParser, FormParser]
+    queryset = Lot.objects.all()
     serializer_class = LotNestedSerializer
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['item__title']
